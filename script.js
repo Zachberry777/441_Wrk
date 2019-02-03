e174f4ecd6459243bfb42bf13e1efb8428118266
Zachary Hockenberry <31712164+Zachberry777@users.noreply.github.com>
2019-02-03 13:31:38 -0700

    test

---
+++ script.js
@@ -0,0 +1,8 @@
+/*
+         Author: Zachary Hockenberry
+         Date: Febuary 2, 2019
+         Purpose: to write to the console
+*/
+
+// This is were i do my top 3 fav resraurants
+console.log(1. lobster)
